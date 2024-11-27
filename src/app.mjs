e201c2823d46e2c67033f07762d80229feaa933f
VolodymyrMikhno <volodymyr3583@gmail.com>
2024-11-27 05:11:12 +0200

    getRatedMovies method

--- src/app.mjs
+++ src/app.mjs
@@ -12,19 +12,24 @@ app.post("/mflix/comments", async (req, res) => {
     res.status(201).end(JSON.stringify(commentDB));
 });
 app.put("/mflix/comments", async (req, res) => {
-    //TODO update comment
-    //req.body {"commentId":<string>, "text":<string>}
+    
+    const comment = await mflixService.updateComment(req.body);
+    res.status(200).end(JSON.stringify(comment));
+
 });
 app.delete("/mflix/comments/:id", async (req, res) => {
-    //TODO delete comment
-   // req.params.id - comment to delete
+    
+   const comment = await mflixService.deleteComment(req.params.id);
+    res.status(200).end(JSON.stringify(comment));
 })
 app.get("/mflix/comments/:id", async (req, res) => {
-    //TODO get comment
-   // req.params.id - comment to get
+   
+   const comment = await mflixService.getComment(req.params.id);
+    res.status(200).end(JSON.stringify(comment));
 })
 app.post("/mflix/movies/rated", async (req, res) => {
-    //TODO find most imdb rated movies
-   // req.body {"year":<number>(optional), "genre":<string>(optional),
-   // "acter":<string-regex>(optional), "amount":<number>(mandatary)}
+    
+   const movies = await mflixService.getRatedMovies(req.body);
+    res.status(200).end(JSON.stringify(movies));
+
 })
--- src/service/MflixService.mjs
+++ src/service/MflixService.mjs
@@ -24,4 +24,66 @@ export default class MflixService {
         const movieId = ObjectId.createFromHexString(commentDto.movie_id);
         return {...commentDto,'movie_id': movieId}
     }
+    async updateComment(commentDto) {
+        const commentId = ObjectId.createFromHexString(commentDto.commentId);
+        const result = await this.#commentsCollection.updateOne(
+            { _id: commentId },
+            { $set: { text: commentDto.text } }
+        );
+        return { _id: commentId, text: commentDto.text };
+    
+    }
+    async deleteComment(id) {
+        const commentId = ObjectId.createFromHexString(id);
+        const result = await this.#commentsCollection.deleteOne({ _id: commentId });
+        return result.deletedCount;
+    }
+    async getComment(id) {
+        const result = await this.#commentsCollection.findOne({_id: ObjectId.createFromHexString(id)});
+        return result;
+    }
+    async getRatedMovies(ratedMoviesDto) {
+        const matchCriteria = {
+            year: ratedMoviesDto.year || { $exists: true },
+            cast: ratedMoviesDto.actor || { $exists: true }
+        };
+        if (ratedMoviesDto.actor) {
+            matchCriteria.cast = { $regex: ratedMoviesDto.actor, $options: "i" }; 
+        }
+
+        const pipeline = [
+            {
+              '$match': matchCriteria
+              
+            }, {
+              '$unwind': {
+                'path': '$genres'
+              }
+            }, {
+              '$unwind': {
+                'path': '$cast'
+              }
+            }, {
+              '$limit': ratedMoviesDto.amount || 10
+            }, {
+              '$sort': {
+                'imdb.rating': -1
+              }
+            }, {
+              '$project': {
+                'title': 1, 
+                'year': 1, 
+                'genres': 1, 
+                'cast': 1, 
+                'imdb.rating': 1, 
+                '_id': 0
+                
+              }
+            }
+          ];
+
+        const result = await this.#moviesCollection.aggregate(pipeline).toArray();
+        return result;
+    }
+
 }

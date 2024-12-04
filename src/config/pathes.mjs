bf90692aa1dedec0d7f777c7131d2f3115f481fb
VolodymyrMikhno <volodymyr3583@gmail.com>
2024-12-04 07:29:21 +0200

    modified:   src/config/pathes.mjs

--- src/config/pathes.mjs
+++ src/config/pathes.mjs
@@ -2,3 +2,5 @@ export const ADD_UPDATE_COMMENT = "/mflix/comments";
 export const DELETE_GET_COMMENT = ADD_UPDATE_COMMENT + "/:id";
 export const GET_MOVIES_RATED = "/mflix/movies/rated";
 export const ADD_ACCOUNT = "/accounts/account"
+export const UPDATE_PASSWORD = `${ADD_ACCOUNT}/password`;
+export const GET_DELETE_ACCOUNT = `${ADD_ACCOUNT}/:username`;

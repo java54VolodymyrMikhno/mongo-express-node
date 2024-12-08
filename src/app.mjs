e9e5f86b2382ce2531a83e3bde7066c740fe9275
VolodymyrMikhno <volodymyr3583@gmail.com>
2024-12-08 18:33:27 +0200

    used rateLimit

--- src/app.mjs
+++ src/app.mjs
@@ -1,11 +1,11 @@
 import express from 'express';
 import { mflix_route } from './routes/mflix.mjs';
-import {validateBody, valid, validateParams} from './middleware/validation.mjs'
+import {validateBody, valid} from './middleware/validation.mjs'
 import { accounts_route } from './routes/accounts.mjs';
 import { errorHandler } from './errors/error.mjs';
 import schemas  from './validation-schemas/schemas.mjs';
 import AccountsService from './service/AccountsService.mjs';
-import { authenticate, auth } from './middleware/authentication.mjs';
+import { authenticate, auth, rateLimit } from './middleware/authentication.mjs';
 import { ADD_UPDATE_ACCOUNT } from './config/pathes.mjs';
 
 const app = express();
@@ -13,13 +13,12 @@ const port = process.env.PORT || 3500;
 export const accountsService = new AccountsService(process.env.MONGO_URI, process.env.DB_NAME);
 const server = app.listen(port);
 
-
 server.on("listening", ()=>console.log(`server listening on port ${server.address().port}`));
 app.use(express.json());
-app.use(authenticate(accountsService));
+app.use(authenticate(accountsService)); 
 app.use(auth(JSON.stringify({path:ADD_UPDATE_ACCOUNT, method:"POST"})))
 app.use(validateBody(schemas));
 app.use(valid);
-app.use('/mflix',mflix_route);
+app.use('/mflix',rateLimit(),mflix_route);
 app.use('/accounts', accounts_route);
 app.use(errorHandler);
